ac8c3025f6cd4eaeaff622b9154f33d0d44a0c5c
AdrianEstevezExposito <aestexp@gmail.com>
2015-11-09 14:11:01 +0000

    Actualizado micarga.py 9/11/2015
    Añadidas funciones para los tipos de dispositivos
    Añadido diccionario para relaciones dispositivo-pin

--- micarga.py
+++ micarga.py
@@ -6,16 +6,10 @@ import sys
 import os
 import time
 
-# Mapear las opciones con su bloque de funcion
-dic_options = {"Actuador" : f_actuadores,
-           "Indicador" : f_indicadores,
-           "Pulsador" : f_pulsadores,
-           "Analogico" : f_analogicos
-}
-
 class modulo:
    nombreModulo = ""
    archivoModulo = ""
+   dic_comandos = {"1":"P0","2":"","3":"","4":"P2","5":"","6":"","7":"P1","8":"","9":"D8","10":"","11":"D4","12":"","13":"","14":"","15":"D5","16":"D6","17":"D3","18":"D2","19":"D1","20":"D0"}
   # pinescargados = [] #Es posible que así esté mal instanciado
    
    
@@ -32,6 +26,7 @@ class modulo:
        self.dicpines_str = {}	# además de otra para la copia en formato str.
        
        self.dicdispositivos = {}	# diccionario en el que guardar el nombre de cada actuador
+       self.dicpines_asignados = {}	# diccionario en el que guardar la asociacion nombre-pin
        break
      
    def cargar(self):
@@ -59,17 +54,128 @@ class modulo:
      self.dicpines_str = nuevodicpines
      return data
    
-   def f_actuadores(self):
-     print "1-Activar actuador \n2-Activar actuador durante cierto tiempo \n3-Desactivar el actuador"
+   def f_actuadores(self, nombre):
+     while True:
+      print "1-Activar actuador \n2-Activar actuador durante cierto tiempo \n3-Desactivar el actuador"
+      try:
+	opt = raw_input("Opción>")
+      except EOFError:
+	print "--Saliendo del programa--"
+	sys.exit(1)
+      if len(opt) == 0:
+	continue
+      if opt == "1":
+	print "Activando actuador.\n"
+      elif opt == "2":
+	print "Activando actuador durante cierto tiempo\n"
+      elif opt == "3":
+	print "Desactivando el actuador\n"
+      else:
+	print "Opción no válida. Opciones válidas: 1 -- 2 -- 3"
+	continue
+      print "El pin sobre el que se actuará será el {}\n".format(self.dicpines_asignados[nombre])
+      pin_actual = self.dicpines_asignados[nombre]
+      if self.dic_comandos[pin_actual] != "":
+	print "Por tanto, el comando utilizado será {}\n".format(self.dic_comandos[pin_actual])
+      else:
+	print "ERROR: No existe comando para trabajar sobre este pin"
+      break
      
-   def f_indicadores(self):
-     print "1-Activar indicador \n2-Activar durante cierto tiempo \n3-Activar con un tipo de parpadeo (tiempo apagado/tiempo encendido) \n4-Desactivar"
-     
-   def f_pulsadores(self):
-     print "1-Evento pulsación simple \n2-Evento pulsación doble \n3-Evento pulsación larga \n4-Evento pulsación muy larga"
-     
-   def f_analogicos(self):
-     print "1-Fijar umbrales mínimo y máximo \n2-Fijar histéresis \n3-Evento zona mínima \n4-Evento zona máxima \n5-Evento zona media"
+   def f_indicadores(self, nombre):
+     while True:
+      print "1-Activar indicador \n2-Activar durante cierto tiempo \n3-Activar con un tipo de parpadeo (tiempo apagado/tiempo encendido) \n4-Desactivar"
+      try:
+	opt = raw_input("Opción>")
+      except EOFError:
+	print "--Saliendo del programa--"
+	sys.exit(1)
+      if len(opt) == 0:
+	continue
+      if opt == "1":
+	print "Activando indicador.\n"
+      elif opt == "2":
+	print "Activando indicador durante cierto tiempo\n"
+      elif opt == "3":
+	print "Parpadeo\n"
+      elif opt == "4":
+	print "Desactivando el indicador\n"
+      else:
+	print "Opción no válida. Opciones válidas: 1 -- 2 -- 3 -- 4"
+	continue
+      print "El pin sobre el que se actuará será el {}\n".format(self.dicpines_asignados[nombre])
+      pin_actual = self.dicpines_asignados[nombre]
+      if self.dic_comandos[pin_actual] != "":
+	print "Por tanto, el comando utilizado será {}\n".format(self.dic_comandos[pin_actual])
+      else:
+	print "ERROR: No existe comando para trabajar sobre este pin"
+      break
+    
+   def f_pulsadores(self, nombre):
+     while True:
+      print "Defina que quiere hacer para: \n1-Evento pulsación simple \n2-Evento pulsación doble \n3-Evento pulsación larga \n4-Evento pulsación muy larga"
+      try:
+	opt = raw_input("Opción>")
+      except EOFError:
+	print "--Saliendo del programa--"
+	sys.exit(1)
+      if len(opt) == 0:
+	continue
+      if opt == "1":
+	print "Evento pulsación simple\n"		#Pedir variable (dispositivo)
+      elif opt == "2":
+	print "Evento pulsación doble\n"		#Pedir variable (dispositivo)
+      elif opt == "3":
+	print "Evento pulsación larga\n"		#Pedir variable (dispositivo)
+      elif opt == "4":
+	print "Evento pulsación muy larga\n"		#Pedir variable (dispositivo)
+      else:
+	print "Opción no válida. Opciones válidas: 1 -- 2 -- 3 -- 4" 
+	continue
+      print "El pin sobre el que se actuará será el {}\n".format(self.dicpines_asignados[nombre])
+      pin_actual = self.dicpines_asignados[nombre]
+      if self.dic_comandos[pin_actual] != "":
+	print "Por tanto, el comando utilizado será {}\n".format(self.dic_comandos[pin_actual])
+      else:
+	print "ERROR: No existe comando para trabajar sobre este pin"
+      break
+    
+   def f_analogicos(self, nombre):
+     while True:
+      print "1-Fijar umbrales mínimo y máximo \n2-Fijar histéresis \n3-Evento zona mínima \n4-Evento zona máxima \n5-Evento zona media"
+      try:
+	opt = raw_input("Opción>")
+      except EOFError:
+	print "--Saliendo del programa--"
+	sys.exit(1)
+      if len(opt) == 0:
+	continue
+      if opt == "1":
+	print "Fijando umbrales.\n"	#Pedir variable (valor actual potenciometro)
+      elif opt == "2":
+	print "Fijando histeresis\n"	#Pedir variable (valor actual potenciometro)
+      elif opt == "3":
+	print "Defina evento de zona minima\n"	#Pedir variable (dispositivo)
+      elif opt == "4":
+	print "Defina evento de zona maxima\n"	#Pedir variable (dispositivo) 
+      elif opt == "5":
+	print "Defina evento de zona media\n"	#Pedir variable (dispositivo)
+      else:
+	print "Opción no válida. Opciones válidas: 1 -- 2 -- 3 -- 4 -- 5"
+	continue
+      print "El pin sobre el que se actuará será el {}\n".format(self.dicpines_asignados[nombre])
+      pin_actual = self.dicpines_asignados[nombre]
+      if self.dic_comandos[pin_actual] != "":
+	print "Por tanto, el comando utilizado será {}\n".format(self.dic_comandos[pin_actual])
+      else:
+	print "ERROR: No existe comando para trabajar sobre este pin"
+      break
+    
+   # Mapear las opciones con su bloque de funcion
+   dic_options = {"Actuador" : f_actuadores,
+           "Indicador" : f_indicadores,
+           "Pulsador" : f_pulsadores,
+           "Analogico" : f_analogicos
+   }
 
 ####################################################################
 ## Programa principal
@@ -78,7 +184,7 @@ if __name__ == "__main__":
   
   device.cargar()
   
-  device.to_str(device.pinescargados)
+  device.to_str(device.pinescargados)	#Necesario convertir los valores en string para mejor manejo
     
   print "Pines cargados convertidos a str: {}".format(device.dicpines_str)
   
@@ -90,7 +196,8 @@ if __name__ == "__main__":
 	print "--Saliendo del programa--"
 	sys.exit(1)
       else:
-	device.dicdispositivos[nombreDisp] = v
+	device.dicpines_asignados[nombreDisp] = k	#guardar el pin
+	device.dicdispositivos[nombreDisp] = v		#guardar el tipo de dispositivo
     else:
       print "Pin {} no asignado".format(k)
       
@@ -98,18 +205,19 @@ if __name__ == "__main__":
   
   while True:
     try:
-      interact = raw_input("Nombre del dispositivo con el que interactuar>")
+      interactuado = raw_input("Nombre del dispositivo con el que interactuar>")
     except EOFError:
       print "--Saliendo del programa--"
       sys.exit(1)
-    if len(interact) == 0:
+    if len(interactuado) == 0:
       continue
     
-    if device.dicdispositivos.has_key(interact):
-      print "El dispositivo '{}' es un {}.\n".format(interact, device.dicdispositivos[interact])
+    if device.dicdispositivos.has_key(interactuado):
+      print "El dispositivo '{}' es un {}.\n".format(interactuado, device.dicdispositivos[interactuado])
     else:
       print "El nombre de dispositivo indicado no existe, pruebe otra vez\n"
       continue
-    print "Para el dispositivo '{}' existen las siguientes acciones:
-    
+    print "Para el dispositivo '{}' existen las siguientes acciones:".format(interactuado)
+    tipo_actual = device.dicdispositivos[interactuado]
+    device.dic_options[tipo_actual](device, interactuado)
     
